8008206c118501584500245c06d66301d8b75ac7
Zoe Rorvig <zrorvig@gmail.com>
2022-12-09 10:40:18 -0500

    populated cards with title, date, description

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var searchButtonEl = document.querySelector('#search-button');
+// var mainSearchButtonEl = document.querySelector('#mainsearch-button');
 var cityInputEl = document.querySelector('#cityInput');
 var selectFormatEl = document.querySelector('#select');
 
@@ -15,6 +16,21 @@ var searchResults = function (event) {
     console.log(cityInputEl.value);
     cityInputEl.value = '';
 };
+
+// var searchResultsMP = function (event) {
+//     event.preventDefault();
+
+//     var location = cityInputEl.value.trim();
+//     var format = selectFormatEl.value;
+
+//     if (format){
+//         getSearchResult(location, format);
+//     }
+
+//     console.log(cityInputEl.value);
+//     cityInputEl.value = '';
+//     window.location.replace("search-results.html");
+// };
 
 var getSearchResult = function(location, format){
     console.log("test");
@@ -40,14 +56,25 @@ var displayResults = function(data, location, format){
 
     for(var i = 0; i < data.length; i++){
         var dataTitle = data[i].title;
-        var cardID = document.querySelector('#card'+ i );
-        console.log(cardID);
-
         console.log(dataTitle);
-        // $('#'+i).children().children('.card-title').val(data[i].title);
+
+        var dataDate = data[i].date;
+        console.log(dataDate);
+
+        var dataDes = data[i].description[0];
+        console.log(dataDes);
+
+        var cardID = "#card" + i;
+        var findCard = document.querySelector(cardID);
+        console.log(findCard);
+
+        findCard.children[0].children[0].textContent = dataTitle;
+        findCard.children[0].children[1].textContent = dataDate;
+        findCard.children[0].children[2].textContent = dataDes;
 
     }
 };
 
 
 searchButtonEl.addEventListener('click', searchResults);
+// mainSearchButtonEl.addEventListener('click', searchResultsMP);
